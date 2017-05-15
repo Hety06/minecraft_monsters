3194e5b191e8f104f19bb02e40eb1e4594297d2f
Hety06 <edelig0929@greenvilleschools.us>
2017-05-15 14:14:51 -0400

    Add files via upload

---
+++ js/fidget_spinners.js
@@ -0,0 +1,78 @@
+function checkIt() {
+  // Initializing variables
+  var oreo = 0
+  var ninja = 0
+  var grass = 0
+  var jesus = 0
+  var oreoimg = "<img src='img2/oreo.png'>"
+  var ninjaimg = "<img src='img2/ninja.png'>"
+  var grassimg = "<img src='img2/grass.png'>"
+  var jesusimg = "<img src='img2/jesus.png'>"
+
+  // Get info from form
+  var elements = document.getElementsByClassName('answer');
+
+  // Tally results
+  for (var e of elements) {
+
+    if (e.checked) {
+      if (e.value == 'oreo') {
+        oreo++;
+      }
+
+      else if (e.value == 'ninja') {
+        ninja++;
+      }
+
+      else if (e.value == 'grass') {
+        grass++;
+      }
+
+      else if (e.value == 'jesus') {
+        jesus++;
+      }
+    }
+  }
+
+  //Build a message
+  var counts = "oreo: " + oreo + ", " +
+               "ninja: " + ninja + ", " +
+               "grass: " + grass + ", " +
+               "jesus: " + jesus;
+
+  var max = Math.max(oreo, ninja, grass, jesus);
+
+  var message;
+
+  if (max == oreo) {
+    message0 = "Which Fidget Spinner Best Fits you?"
+    message1 = "OREO SPINNER!!"
+    message2 = "You are an amazing person with great opinions and thoughts!"
+    message3 = oreoimg
+  }
+  else if (max == ninja) {
+    message0 = "Which Fidget Spinner Best Fits you?"
+    message1 = "NINJA SPINNER!!"
+    message2 = "You're super sneaky and your choices are cunning and crazy."
+    message3 = ninjaimg
+  }
+  else if (max == grass) {
+    message0 = "Which Fidget Spinner Best Fits you?"
+    message1 = "APRIL 20TH SPINNER!!"
+    message2 = "You love hanging out with the boys (or gals) and you always have a great time!"
+    message3 = grassimg
+  }
+  else if (max == jesus) {
+    message0 = "Which Fidget Spinner Best Fits you?"
+    message1 = "JESUS SPINNER!!"
+    message2 = "You are so in tune with the lord and you hate wacky tabacky!"
+    message3 = jesusimg
+  }
+
+  //DIsplay message
+  //document.getElementById('result').innerHTML = counts;
+  document.getElementById('result-title').innerHTML = message0;
+  document.getElementById('result-heading').innerHTML = message1;
+  document.getElementById('result-text').innerHTML = message2;
+  document.getElementById('result-img').innerHTML = message3;
+}
---
+++ js/minecraft_monsters.js
@@ -0,0 +1,78 @@
+function checkIt() {
+  // Initializing variables
+  var creeper = 0
+  var skeleton = 0
+  var spider = 0
+  var zombie = 0
+  var creeperimg = "<img src='img/creeper.png'>"
+  var spiderimg = "<img src='img/spider.png'>"
+  var skeletonimg = "<img src='img/skeleton.png'>"
+  var zombieimg = "<img src='img/zombie.png'>"
+
+  // Get info from form
+  var elements = document.getElementsByClassName('answer');
+
+  // Tally results
+  for (var e of elements) {
+
+    if (e.checked) {
+      if (e.value == 'creeper') {
+        creeper++;
+      }
+
+      else if (e.value == 'skeleton') {
+        skeleton++;
+      }
+
+      else if (e.value == 'spider') {
+        spider++;
+      }
+
+      else if (e.value == 'zombie') {
+        zombie++;
+      }
+    }
+  }
+
+  //Build a message
+  var counts = "Creeper: " + creeper + ", " +
+               "Skeleton: " + skeleton + ", " +
+               "Spider: " + spider + ", " +
+               "Zombie: " + zombie;
+
+  var max = Math.max(creeper, skeleton, spider, zombie);
+
+  var message;
+
+  if (max == creeper) {
+    message0 = "Which Minecraft monster are you?"
+    message1 = "YOU ARE A CREEPER!!"
+    message2 = "You enjoy hiding behind trees and waiting until a harmless victim comes by to EXPLODE!"
+    message3 = creeperimg
+  }
+  else if (max == skeleton) {
+    message0 = "Which Minecraft monster are you?"
+    message1 = "OH NO YOU'RE A SKELETON!!"
+    message2 = "You like waiting in the dark and hiding behind rocks with your bow and arrow."
+    message3 = skeletonimg
+  }
+  else if (max == spider) {
+    message0 = "Which Minecraft monster are you?"
+    message1 = "EEK YOU ARE A SPIDER!!"
+    message2 = "You climb all over the place and wait until your target is right under you before you STRIKE!"
+    message3 = spiderimg
+  }
+  else if (max == zombie) {
+    message0 = "Which Minecraft monster are you?"
+    message1 = "YOU'RE A ZOMBIE!!"
+    message2 = "You love eating villagers and tasty brains while walking in a deep dark forest."
+    message3 = zombieimg
+  }
+
+  //DIsplay message
+  //document.getElementById('result').innerHTML = counts;
+  document.getElementById('result-title').innerHTML = message0;
+  document.getElementById('result-heading').innerHTML = message1;
+  document.getElementById('result-text').innerHTML = message2;
+  document.getElementById('result-img').innerHTML = message3;
+}
